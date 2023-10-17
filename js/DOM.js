f007736b99eb6329defbc18992fe987baf59d0bf
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-10-17 12:27:29 -0500

    Avanzamos algunas pestañas hoy, falta terminar la estilizacion de la pestaña agregar curso

--- js/DOM.js
+++ js/DOM.js
@@ -1,13 +1,40 @@
 const link_button_registro = document.querySelector('.container-section__button--registro')
 const link_button_sesion = document.querySelector('.container-section__button--inicio-sesion')
+const BUTTON_INICIAR_SESION = document.querySelector('.container-form__button--entrar-ingresar')
+const BUTTON_REGISTRAR_USUARIO = document.querySelector('.container-form__button--entrar-registrar')
+const BUTTON_AGREGAR_CURSO = document.querySelector('.container-section__agregar-curso')
 
 const url_iniciar_sesion = '/iniciar_sesion.html'
 const url_registro = '/registrar_usuario.html'
+const URL_BIENVENIDA_USUARIO = '/usuario_bienvenida.html'
+const URL_AGREGAR_CURSO = '/agregar_curso.html'
 
-link_button_sesion.addEventListener('click', ()=>{
-  window.location.href = url_iniciar_sesion
-})
+if (link_button_sesion) {
+  link_button_sesion.addEventListener('click', ()=>{
+    window.location.href = url_iniciar_sesion
+  })
+}
 
-link_button_registro.addEventListener('click', ()=> {
-  window.location.href = url_registro
-})
+if (link_button_registro) {
+  link_button_registro.addEventListener('click', ()=> {
+    window.location.href = url_registro
+  })
+}
+
+if (BUTTON_INICIAR_SESION) {
+  BUTTON_INICIAR_SESION.addEventListener('click', ()=>{
+    window.location.href = URL_BIENVENIDA_USUARIO
+  })
+}
+
+if (BUTTON_REGISTRAR_USUARIO) {
+  BUTTON_REGISTRAR_USUARIO.addEventListener('click', ()=> {
+    window.location.href = URL_BIENVENIDA_USUARIO
+  })
+}
+
+if (BUTTON_AGREGAR_CURSO) {
+  BUTTON_AGREGAR_CURSO.addEventListener('click', ()=> {
+    window.location.href = URL_AGREGAR_CURSO
+  })
+}
