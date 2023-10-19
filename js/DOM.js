22cdcfcf065fa90c7f7bec232497cd8690ca467b
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2023-10-19 12:25:17 -0500

    terminado de maquetar

--- js/DOM.js
+++ js/DOM.js
@@ -4,12 +4,14 @@ const BUTTON_INICIAR_SESION = document.querySelector('.container-form__button--e
 const BUTTON_REGISTRAR_USUARIO = document.querySelector('.container-form__button--entrar-registrar')
 const BUTTON_AGREGAR_CURSO = document.querySelector('.container-section__agregar-curso')
 const OPCION_EDITAR = document.querySelector('.container-item-personalizado--opciones-editar')
+const GUARDAR_CAMBIOS_EDITAR = document.querySelector('.container-form__button--editar-guardar')
 
 const url_iniciar_sesion = '/iniciar_sesion.html'
 const url_registro = '/registrar_usuario.html'
 const URL_BIENVENIDA_USUARIO = '/usuario_bienvenida.html'
 const URL_AGREGAR_CURSO = '/agregar_curso.html'
 const URL_EDITAR_USUARIO = '/administrador_usu.html'
+const URL_ADMINISTRADOR_BIENVENIDA = '/administrador_bienvenida.html'
 
 if (link_button_sesion) {
   link_button_sesion.addEventListener('click', ()=>{
@@ -44,5 +46,11 @@ if (BUTTON_AGREGAR_CURSO) {
 if (OPCION_EDITAR) {
   OPCION_EDITAR.addEventListener('click', ()=>{
     window.location.href = URL_EDITAR_USUARIO
+  })
+}
+
+if (GUARDAR_CAMBIOS_EDITAR) {
+  GUARDAR_CAMBIOS_EDITAR.addEventListener('click', ()=> {
+    window.location.href = URL_ADMINISTRADOR_BIENVENIDA
   })
 }
